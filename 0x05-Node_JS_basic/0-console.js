f213601e36a954db7cd2748fc09458f7d50e4e1b
Yusuf Yetunde <yusufyetunde13@gmail.com>
2024-05-01 10:01:39 +0000

    node task

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,6 +1,6 @@
 /*create a function named displayMessage that prints in STDOUT the string argument*/
 
 function displayMessage(string){
-console.log(string)
+console.log(string);
 }
 module.exports = displayMessage;
